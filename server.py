84b64089c0410275f3ddc713582b52ab90d1eb26
Timur Batyrshin <erthad@gmail.com>
2013-09-23 23:37:15 +0400

    indentation fix

--- server.py
+++ server.py
@@ -15,30 +15,30 @@ class Tokenizer:
       self.secret = self._hex(secret)
 
     def generate(self, length = default_length):
-    '''Generate and store random token'''
+      '''Generate and store random token'''
 
       token = self._hex(os.urandom(length))
       self.tokens.append(self.encode(token))
       return token
 
     def check(self, token):
-    '''Check if token is correct (when passed unencrypted)'''
+      '''Check if token is correct (when passed unencrypted)'''
       token = self.encode(token)
       return self.check_encoded(token)
 
     def check_encoded(self, token):
-    '''Check if token is correct (when passed encrypted)'''
+      '''Check if token is correct (when passed encrypted)'''
       return self.tokens.index(token)
 
     def encode(self,token):
-    '''Encode hex string'''
+      '''Encode hex string'''
       result = ""
       for idx,char in enumerate(token):
         result += chr(int(char,16) ^ int((self.secret*100)[idx],16))
       return self._hex(result)
 
     def _hex(self,s):
-    '''Convert string to hex chars'''
+      '''Convert string to hex chars'''
       hexnum =""
       for i in s:
         hexnum +=  string.replace(hex(ord(i)),"0x","")  # generate string of random hex digits
